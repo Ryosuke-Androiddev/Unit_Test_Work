1027d347a34d2c987fe1c0583472362a6c6ec413
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2023-04-11 07:46:06 +0900

    HiltのEntryPointを追加

---
+++ app/src/main/java/com/example/unit_test_work/BaseApplication.kt
@@ -0,0 +1,8 @@
+package com.example.unit_test_work
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+}
--- app/src/main/java/com/example/unit_test_work/MainActivity.kt
+++ app/src/main/java/com/example/unit_test_work/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unit_test_work.ui.theme.Unit_Test_WorkTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
