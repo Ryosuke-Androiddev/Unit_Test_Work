ce197bfb689b386278f55fd3b190713a6ba0a8e5
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2023-04-11 07:47:21 +0900

    Database周りの処理の記述変更

--- app/src/main/java/com/example/unit_test_work/data/db/TodoDatabase.kt
+++ app/src/main/java/com/example/unit_test_work/data/db/TodoDatabase.kt
@@ -1,15 +1,16 @@
 package com.example.unit_test_work.data.db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.unit_test_work.domain.model.TodoItem
 
 @Database(
     entities = [TodoItem::class],
     version = 1
 )
-abstract class TodoDatabase {
+abstract class TodoDatabase: RoomDatabase() {
 
-    abstract val todoDao: TodoDao
+    abstract fun todoDao(): TodoDao
 
     companion object {
         const val DATABASE_NAME = "todo_db"
--- app/src/main/java/com/example/unit_test_work/di/DatabaseModule.kt
+++ app/src/main/java/com/example/unit_test_work/di/DatabaseModule.kt
@@ -1,5 +1,8 @@
 package com.example.unit_test_work.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.unit_test_work.data.db.TodoDao
 import com.example.unit_test_work.data.db.TodoDatabase
 import com.example.unit_test_work.data.repository.TodoRepositoryImpl
 import com.example.unit_test_work.domain.repository.TodoRepository
@@ -15,8 +18,22 @@ object DatabaseModule {
 
     @Provides
     @Singleton
-    fun provideTodoRepository(db: TodoDatabase): TodoRepository {
+    fun provideTodoDatabase(app: Application): TodoDatabase {
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            TodoDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideDao(database: TodoDatabase) = database.todoDao()
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(dao: TodoDao): TodoRepository {
         // Domain層の抽象に依存した(Interfaceを実装した)クラスを返す
-        return TodoRepositoryImpl(db.todoDao)
+        return TodoRepositoryImpl(dao)
     }
 }
